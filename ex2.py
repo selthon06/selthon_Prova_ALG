74380e8beb207bd682c42d7793112c385bf0fa18
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-07-01 21:01:34 -0400

    Atualizar o ex2.py
    .

--- ex2.py
+++ ex2.py
@@ -1,1 +1,15 @@
-print('Ola mundo')
+alunos = {
+    'Ana': [1, 1, 0, 1, 1],
+    'Bruno': [1, 0, 0, 1, 0],
+    'Clara': [1, 1, 1, 1, 1],
+    'Daniel': [0, 0, 1, 1, 1]
+}
+
+aprovados = []
+
+for nome, presencas in alunos.items():
+    taxa_presenca = sum(presencas) / len(presencas)
+    if taxa_presenca >= 0.7:
+        aprovados.append(nome)
+
+print("Alunos aprovados:", aprovados)
