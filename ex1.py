d9ce8fa2746819261a04539f942e0f0dcd06e3db
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-07-01 21:00:50 -0400

    Atualizar o ex1.py
    .

--- ex1.py
+++ ex1.py
@@ -1,1 +1,13 @@
-print('Ola mundo')
+import random
+
+setores = {
+    'Administrativo': ['Ana', 'João', 'Marcos'],
+    'Operacional': ['Carlos', 'Bruna', 'Rita'],
+    'Técnico': ['Lucas', 'Fernanda', 'Pedro']
+}
+
+setor_sorteado = random.choice(list(setores.keys()))
+funcionario_sorteado = random.choice(setores[setor_sorteado])
+
+print(f'Setor sorteado: {setor_sorteado}')
+print(f'Funcionário sorteado: {funcionario_sorteado}')
