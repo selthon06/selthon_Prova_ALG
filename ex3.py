1ff7368ddec30006a462cb9a520e215fc06e4cd0
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-07-01 21:03:05 -0400

    Atualizar o ex3.py
    .

--- ex3.py
+++ ex3.py
@@ -1,1 +1,16 @@
-print('Ola mundo')
+import random
+
+def aplicar_desconto(valor):
+    sorteio = random.randint(1, 100)
+    if sorteio <= 30:
+        desconto = 0.10
+    elif sorteio <= 70:
+        desconto = 0.25
+    else:
+        desconto = 0.50
+    valor_final = valor * (1 - desconto)
+    return f'{valor_final:,.2f}'.replace('.', ',')  # formata com vírgula
+
+# Exemplo:
+valor_original = 100.0
+print("Valor com desconto aplicado:", aplicar_desconto(valor_original))
